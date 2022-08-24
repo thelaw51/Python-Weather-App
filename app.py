aa4b68959fbfd91076708c15c02c2210aa548bf1
joe gillard <joegillard3@gmail.com>
2022-08-24 08:11:14 +0100

    Added print lines to print weather type and temp and to print status code

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ userInput = input("Enter city or town: ")
 
 weatherData = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={userInput}&units=metric&APPID={apiKey}")
 
+weather = weatherData.json()['weather'][0]['main']
+temp =round(weatherData.json()['main']['temp'])
 
-print(weatherData.json())
+print(f"The weather in {userInput} is: {weather}")
+print(f"The temperature in {userInput} is: {temp}°C")
+print(f"api call return code: {weatherData.status_code}")
+
